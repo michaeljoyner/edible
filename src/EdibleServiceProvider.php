fcbc99863d502123f9da6906680f2b01241e3991
Michael Joyner <joyner.michael@gmail.com>
2016-03-03 22:31:14 +0800

    still working

--- src/EdibleServiceProvider.php
+++ src/EdibleServiceProvider.php
@@ -10,6 +10,7 @@ class EdibleServiceProvider extends ServiceProvider
 {
 
     protected $defer = true;
+
     /**
      * Bootstrap the application services.
      *
@@ -27,11 +28,18 @@ class EdibleServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(ContentWriter::class, function($app) {
+        $this->app->bind(ContentWriter::class, function ($app) {
             $fileSnapshot = (new ContentSnapshotFactory(new Parser()))->makeSnapshotFromYmlFile(base_path('edible.yml'));
             $databaseSnapshot = (new ContentSnapshotFactory(new Parser()))->makeSnapshotFromRepo(new ContentRepository());
+
             return new ContentWriter($fileSnapshot, $databaseSnapshot);
         });
+
+        $this->publishes([
+            __DIR__ . '/../resources/migrations/2016_01_26_004711_create_pages_table.php' => database_path('migrations/2016_01_26_004711_create_pages_table.php'),
+            __DIR__ . '/../resources/migrations/2016_01_26_004750_create_textblocks_table.php' => database_path('migrations/2016_01_26_004750_create_textblocks_table.php'),
+        __DIR__ . '/../resources/migrations/2016_01_26_004801_create_galleries_table.php' => database_path('migrations/2016_01_26_004750_create_textblocks_table.php'),
+        ], 'migrations');
 
         $this->commands(['Michaeljoyner\Edible\Console\MapContentStructure']);
     }
