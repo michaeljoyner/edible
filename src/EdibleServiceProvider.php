d25127acc397f96f41110efda8a326dabce62228
Michael Joyner <joyner.michael@gmail.com>
2016-03-05 09:42:11 +0800

    add view composer to service provider, and clean up

--- src/EdibleServiceProvider.php
+++ src/EdibleServiceProvider.php
@@ -19,11 +19,18 @@ class EdibleServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../resources/migrations/2016_01_26_004711_create_pages_table.php' => database_path('migrations/2016_01_26_004711_create_pages_table.php'),
-            __DIR__ . '/../resources/migrations/2016_01_26_004750_create_textblocks_table.php' => database_path('migrations/2016_01_26_004750_create_textblocks_table.php'),
-            __DIR__ . '/../resources/migrations/2016_01_26_004801_create_galleries_table.php' => database_path('migrations/2016_01_26_004801_create_galleries_table.php'),
-        ], 'migrations');
+        $this->publishesMigrations();
+        $this->publishesConfig();
+
+        if(config('edible.menu_view')) {
+            view()->composer(config('menu_view'), function ($view) {
+                $ediblePages = (new ContentRepository())->getPageListWithUrls();
+
+                return $view->with(compact('ediblePages'));
+            });
+        }
+
+
     }
 
     /**
@@ -33,20 +40,34 @@ class EdibleServiceProvider extends ServiceProvider
      */
     public function register()
     {
-//        $this->app->bind(ContentWriter::class, function ($app) {
-//            $fileSnapshot = (new ContentSnapshotFactory(new Parser()))->makeSnapshotFromYmlFile(base_path('edible.yaml'));
-//            $databaseSnapshot = (new ContentSnapshotFactory(new Parser()))->makeSnapshotFromRepo(new ContentRepository());
-//
-//            return new ContentWriter($fileSnapshot, $databaseSnapshot);
-//        });
-
-
-
         $this->commands(['Michaeljoyner\Edible\Commands\MapContentStructure']);
     }
 
     public function provides()
     {
         return [ContentWriter::class];
+    }
+
+    private function publishesMigrations()
+    {
+        $filesMap = [];
+        $resourcePath = __DIR__ . '/../resources/migrations/';
+        $files = [
+            '2016_01_26_004711_create_pages_table.php',
+            '2016_01_26_004750_create_textblocks_table.php',
+            '2016_01_26_004801_create_galleries_table.php'
+        ];
+
+        foreach ($files as $file) {
+            $filesMap[$resourcePath . $file] = database_path('migrations/' . $file);
+        }
+
+        $this->publishes($filesMap, 'migrations');
+    }
+
+    private function publishesConfig()
+    {
+        $resourcePath = __DIR__ . '/../resources/config/';
+        $this->publishes([$resourcePath . 'edible.php' => config_path('edible.php')], 'config');
     }
 }
